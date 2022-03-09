fc11b2125a60327e6db13fe0e28c05dcfda41109
Giorgos Logiotatidis <seadog@sealabs.net>
2022-03-09 23:10:58 +0200

    Let it shine.

---
+++ generate-rss.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -xe
+
+USERNAMES="panospet"
+
+mkdir -vp rss
+
+generate_yt_cmd() {
+    JSON_DATA=$(mktemp --suffix .json)
+    YT_CMD="youtube-dl -f http https://www.mixcloud.com/${user}/ -J --skip-download > ${JSON_DATA}"
+}
+
+for user in ${USERNAMES}
+do
+    generate_yt_cmd
+    eval "${YT_CMD}"
+
+    jinja render -d "${JSON_DATA}" -t ./template.yaml -lb -tb  > "rss/${user}.rss"
+done
