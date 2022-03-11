f8df046f3340dc560157fa9bb5e5f9a8122b2e9c
Giorgos Logiotatidis <seadog@sealabs.net>
2022-03-11 11:48:47 +0200

    Run script.

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -xe
+
+git push --set-upstream origin master
+python3 -m venv venv
+. venv/bin/activate
+pip install -r requirements.txt
+./generate-rss.sh
+git add docs
+git commit -m "New RSS"
+git push origin
+curl -s https://hc-ping.com/810f622a-404e-4d58-9d98-f659426c37b5
