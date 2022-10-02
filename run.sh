057afe9dd438151858aa06b03559c208089172ef
Giorgos Logiotatidis <seadog@sealabs.net>
2022-10-02 18:26:12 +0300

    Only push on new files.

--- run.sh
+++ run.sh
@@ -6,7 +6,13 @@ python3 -m venv venv
 . venv/bin/activate
 pip install -r requirements.txt
 ./generate-rss.sh
-git add docs
-git commit -m "New RSS"
-git push origin
+
+NEW_FILES=$(git diff --quiet || echo "yes")
+
+if [ "${NEW_FILES}" = "yes" ]
+then
+    git add docs
+    git commit -m "New RSS"
+    git push origin
+fi
 curl -s https://hc-ping.com/810f622a-404e-4d58-9d98-f659426c37b5
