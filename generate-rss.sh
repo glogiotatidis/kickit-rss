b9b72f83efc7a4f334d917e2cfa51dab0d9a08d9
Giorgos Logiotatidis <seadog@sealabs.net>
2022-10-02 18:15:00 +0300

    New generator.

--- generate-rss.sh
+++ generate-rss.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 set -xe
 
-USERNAMES="panospet kickitathens"
+USER="kickitathens"
+NEW_RSS=$(mktemp --suffix .rss)
+MIXCLOUD_JSON_DATA=$(mktemp --suffix .json)
+FORCE=false
 
-generate_yt_cmd() {
-    JSON_DATA=$(mktemp --suffix .json)
-    YT_CMD="youtube-dl --geo-bypass -f http https://www.mixcloud.com/${user}/ -J --skip-download | jq '.entries[].timestamp |= strftime(\"%a, %d %b %Y %H:%M:%S +0000\")' > ${JSON_DATA}"
-}
-
-for user in ${USERNAMES}
-do
-    generate_yt_cmd
-    eval "${YT_CMD}"
-
-    jinja render -d "${JSON_DATA}" -t ./template.yaml -lb -tb  > "docs/${user}.rss"
+while getopts "f" arg; do
+  case $arg in
+    f)
+      FORCE=true
+      ;;
+  esac
 done
+
+mkdir -vp docs
+
+# Fetch mixcloud data
+youtube-dl --geo-bypass -f http https://www.mixcloud.com/${USER}/ -J --skip-download | jq '.entries[].timestamp |= strftime("%a, %d %b %Y %H:%M:%S +0000")' > ${MIXCLOUD_JSON_DATA}
+
+# GENERATE RSS
+jinja render -d "${MIXCLOUD_JSON_DATA}" -t ./template.yaml -lb -tb  > ${NEW_RSS}
+
+# Check if contents of file are newer
+CURRENT_DATE=$(date --date "$(cat docs/${USER}.rss |  grep -oP "<pubDate>\K(.+)(?=</pubDate>)" | head -n 1)" +%s)
+NEW_DATE=$(date --date "$(cat ${NEW_RSS} |  grep -oP "<pubDate>\K(.+)(?=</pubDate>)" | head -n 1)" +%s)
+
+if [ ${FORCE} = true ] || [[ ${NEW_DATE} -gt ${CURRENT_DATE} ]]
+then
+   echo "New file moving"
+   mv ${NEW_RSS} docs/${USER}.rss
+fi
